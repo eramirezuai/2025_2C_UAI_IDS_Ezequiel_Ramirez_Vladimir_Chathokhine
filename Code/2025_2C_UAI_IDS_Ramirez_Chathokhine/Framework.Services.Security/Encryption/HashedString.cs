c278d7501ed323d19ab6dc1927b7d37533dd3670
Your Name <you@example.com>
2025-09-30 23:42:15 -0300

    Se agrega servicio de Seguridad al proyecto

---
+++ Code/2025_2C_UAI_IDS_Ramirez_Chathokhine/Framework.Services.Security/Encryption/HashedString.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Framework.Services.Security.Encryption
+{
+    public class HashedString
+    {
+        private string valorHasheado;
+        private IHasher hasher;
+
+        public HashedString(string password) : this(password, new SHA256Hasher())
+        {
+        }
+
+        public HashedString(string password, IHasher hasher)
+        {
+            this.hasher = hasher;
+            this.valorHasheado = hasher.Hash(password);
+        }
+
+        public string ValorHasheado
+        {
+            get { return valorHasheado; }
+        }
+
+        public override string ToString()
+        {
+            return ValorHasheado;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is HashedString)
+                return this.ValorHasheado.Equals(((HashedString)obj).ValorHasheado);
+            else if (obj is String)
+                return this.ValorHasheado.Equals(hasher.Hash((string)obj));
+            else return this.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return valorHasheado.GetHashCode();
+        }
+
+        public static bool operator ==(HashedString hash, string valor)
+        {
+            return hash.Equals(valor);
+        }
+
+        public static bool operator !=(HashedString hash, string valor)
+        {
+            return !hash.Equals(valor);
+        }
+
+        public static bool operator ==(string value, HashedString valor)
+        {
+            return valor.Equals(value);
+        }
+
+        public static bool operator !=(string value, HashedString valor)
+        {
+            return !valor.Equals(value);
+        }
+    }
+}
