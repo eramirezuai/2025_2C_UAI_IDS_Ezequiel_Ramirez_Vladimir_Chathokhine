82ac9ad877bfdab121e7a517e17e5c0cb66b9cd7
Vladimir Chatokhine <vladimir.chatokhine@gmail.com>
2025-09-30 23:33:05 -0300

    Agregados componentes de login

--- Code/2025_2C_UAI_IDS_Ramirez_Chathokhine/GUI/FormLogin.cs
+++ Code/2025_2C_UAI_IDS_Ramirez_Chathokhine/GUI/FormLogin.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class FormLogin : Form
     {
-        public Form1()
+        public FormLogin()
         {
             InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
