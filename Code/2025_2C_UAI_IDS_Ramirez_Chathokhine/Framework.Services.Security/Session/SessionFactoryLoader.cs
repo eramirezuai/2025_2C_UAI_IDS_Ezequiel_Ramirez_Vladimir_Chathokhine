64e5ca6f4b80493ed9d532328c81e1aec5318fdd
Ezequiel Ramirez <EzequielFabian.Ramirez@alumnos.uai.edu.ar>
2025-10-08 21:22:01 -0300

    Se agrega estructura singleton para el manejo de sesiones

---
+++ Code/2025_2C_UAI_IDS_Ramirez_Chathokhine/Framework.Services.Security/Session/SessionFactoryLoader.cs
@@ -0,0 +1,14 @@
+﻿using Framework.DAL;
+using Framework.Services.Security.Credentials;
+
+namespace Framework.Services.Security
+{
+    public class SessionFactoryLoader
+    {
+        internal static void LoadClasses()
+        {
+            SessionFactory.ConfigureType<IUser>(() => new User());
+            SessionFactory.ConfigureType<IUserRetrieverByCredentials>(() => new UserCrud(new Access(), new UserParameterMapper()));
+        }
+    }
+}
